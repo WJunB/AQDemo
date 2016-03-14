b62c2ef7feb337eb16c2c24b6669981635ad85f2
jun <jun@CHENdeMac-mini.local>
2016-03-14 19:49:12 +0800

    audioqueue project build first

---
+++ audioqueue/AQDemo/AQDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AQDemo
+//
+//  Created by jun on 16/3/14.
+//  Copyright © 2016年 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ audioqueue/AQDemo/AQDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AQDemo
+//
+//  Created by jun on 16/3/14.
+//  Copyright © 2016年 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
