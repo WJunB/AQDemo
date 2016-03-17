42b6b5520221271bb4f697a554272ab2029d2f4c
jun <jun@CHENdeMac-mini.local>
2016-03-17 13:30:10 +0800

    audio unit demo, could run

---
+++ audiounit/AUDemo/AUDemo/RootViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  AUDemo
+//
+//  Created by jun on 16/3/15.
+//  Copyright © 2016年 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import "InMemoryAudioFile.h"
+
+@interface RootViewController : UIViewController{
+    AudioStreamBasicDescription mAudioFormat;
+    AudioComponentInstance      mAudioUnit;
+    ExtAudioFileRef             mAudioFileRef;
+    Boolean                     isRecording;
+    Boolean                     isPlaying;
+    InMemoryAudioFile           *inMemoryAudioFile;
+}
+
+
+@end
+
